dc736ece2733bc75fb5bab2e06c2e6947fa4abfa
Valeri Hristov <valericfbg@gmail.com>
2021-03-15 23:57:53 +0200

    Initial project structure

---
+++ src/main/java/com/valeri/programs/solver/Driver.java
@@ -0,0 +1,7 @@
+package com.valeri.programs.solver;
+
+final public class Driver {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ src/test/java/WordTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class WordTest {
+    @Test
+    public void test() {
+        System.out.println("Hello, Test!");
+    }
+}
